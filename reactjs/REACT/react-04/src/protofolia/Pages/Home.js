463627dd202d9a567f763ed6567e03bb74135725
nunnanmanasa682 <nunnanmanasa682@gmail.com>
2023-12-19 12:30:05 +0530

    msg

--- reactjs/REACT/react-04/src/protofolia/Pages/Home.js
+++ reactjs/REACT/react-04/src/protofolia/Pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2> Hi, My Name is Nunna Manasa</h2>
+        <h2> Hi,This is  Nunna Manasa</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
           <LinkedInIcon />
